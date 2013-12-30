de3b7cd97a396a8c3182f8ba8acf1bc4b71da516
gghez <gregory.ghez@gmail.com>
2013-12-30 18:48:12 +0100

    Use """ instead of multiple escaping char \ inside html bloc

--- Script.fsx
+++ Script.fsx
@@ -6,34 +6,21 @@
 
 open FsWebAgility.WebAgility
 
-let html = "< div    data-key=\"toto\"
-                    data-model viewmodel='some \"view\" model'
+let html = """< div    data-key="toto"
+                    data-model viewmodel='some \'view\' model'
                     width   = 650
                     data   =
-                        \"titi\" last=some>
+                        "titi" last=some>
                 <h1>Title <span class='b'>te<b>x</b>t </span></h1><hr/>
-                <p style=\"font-style: italic;\">Content text</p><p>second content</p>
-            </ div > <!--This is a comment bloc --><section class=\"sec\" /> "
+                <p style="font-style: \'italic\';">Content text</p><p>second content</p>
+            </ div > <!--This is a comment bloc --><section class=sec/> """
+
+// Failed test fixing in progress...
+//let html = """
+//              <script type='text/javascript'>/*<![CDATA[*/var thickboxL10n={"next":"Suiv.\u00a0>","prev":"<\u00a0Pr\u00e9c.","image":"Image","of":"sur","close":"Fermer","noiframes":"Cette fonctionnalit\u00e9 requiert des iframes. Les iframes sont d\u00e9sactiv\u00e9es sur votre navigateur, ou alors il ne les accepte pas.","loadingAnimation":"http:\/\/leprofdinfo.fr\/wp-includes\/js\/thickbox\/loadingAnimation.gif","closeImage":"http:\/\/leprofdinfo.fr\/wp-includes\/js\/thickbox\/tb-close.png"};/*]]>*/</script>"""
 
 
-let elements = ReadElements html
-
-type Tag(name:string, attrs:Attribute list, childs: Tag list) =
-    new(name:string) = Tag(name, [], [])
-
-let rec FindElements eltname (elements:Element list) =
-    match elements with
-    | elt::after    -> 
-        match elt with
-        | NodeDefinition(name, attrs, autoclosed) when name = eltname   -> NodeDefinition(name, attrs, autoclosed)::FindElements eltname after
-        | _                                                             -> FindElements eltname after
-    | _             -> []
-
-let paragraphs = FindElements "p" elements
-
-paragraphs |> Seq.iter (fun p -> printfn "%A" p)
+HtmlNodes html |> Seq.iter (fun p -> printfn "%A" p)
+HtmlTags "b" html |> Seq.iter (fun p -> printfn "%A" p)
 
 
-
-//ReadHtml "http://leprofdinfo.fr"
-//    |> Async.RunSynchronously
