b9e64621e2dda6dd7bfea87f4c60cb0dcb1fd05f
gghez <gregory.ghez@gmail.com>
2013-12-31 19:07:17 +0100

    Bug fix on reading <script> tags

--- WebAgility.fs
+++ WebAgility.fs
@@ -15,39 +15,41 @@ module WebAgility =
             (first + text, afterText)
         | _                             -> (System.String.Empty, input)
 
-    // Read chars located into a literal string until next string delimiter : sep
-    let rec ReadIntoString sep input =
-        let escaptedSep = "\\" + sep
-        match input with
-        // Match input end (so string end)
-        | IsEmpty                   -> (System.String.Empty, System.String.Empty)
-        // Match explicit end
-        | Prefix sep after          -> (System.String.Empty, after)
-        // Match escaped sep to avoid closing string before real end
-        | Prefix escaptedSep after  ->
-            let (str,safter) = ReadIntoString sep after
-            (escaptedSep + str, safter)
-        | _                         ->
-            let (str, safter) = ReadIntoString sep (TrimFirst input)
-            ((FirstChar input) + str, safter)
+    // Read chars located into a literal string until next string delimiter
+    let ReadString input =
+        let rec ReadIntoDelimitedString sep input =
+            let escaptedSep = "\\" + sep
+            match input with
+            // Match input end (so string end)
+            | IsEmpty                   -> (System.String.Empty, System.String.Empty)
+            // Match explicit end
+            | Prefix sep after          -> (System.String.Empty, after)
+            // Match escaped sep to avoid closing string before real end
+            | Prefix escaptedSep after  ->
+                let (str,safter) = ReadIntoDelimitedString sep after
+                (escaptedSep + str, safter)
+            | _                         ->
+                let (str, safter) = ReadIntoDelimitedString sep (TrimFirst input)
+                ((FirstChar input) + str, safter)
+        ReadIntoDelimitedString (FirstChar input) (TrimFirst input)
 
     // Read an attribute value (string or literal) from input
     let ReadAttributeValue input =
         match input with
         // String detected starting with a single quote
-        | Prefix "'" after              -> ReadIntoString "'" after
+        | Prefix "'" after
         // String detected starting with a double quote
-        | Prefix @"""" after            -> ReadIntoString @"""" after
+        | Prefix @"""" after            -> ReadString input
         // String guess without any quotes -> read to first whitespace char
         | _                             -> ReadIntoLiteral input
 
     // Try match a node name or attribute name char from input
     let (|NameChar|_|) input =
         match input with
-        | LetterOrDigit (l, after)  -> Some(l, after)
-        | Prefix "_" after          -> Some("_", after)
-        | Prefix "-" after          -> Some("-", after)
-        | _                         -> None
+        | PrefixFirstLetterOrDigit (l, after)   -> Some(l, after)
+        | Prefix "_" after                      -> Some("_", after)
+        | Prefix "-" after                      -> Some("-", after)
+        | _                                     -> None
 
     // Try match first char of html with a known attribute char delimiter
     let (|AttributeNameDelimiter|_|) html =
@@ -154,11 +156,11 @@ module WebAgility =
 
     // Provides a simple structure for HTML element
     type HtmlNode =
-        | NodeDefinition of string * Attribute list * bool
-        | NodeClosing of string
-        | Literal of string
-        | Comment of string
-        | Unknown of string
+        | NodeDefinition of string * Attribute list * bool // name, attributes, autoclosed
+        | NodeClosing of string // name
+        | Literal of string // content
+        | Comment of string // comment
+        | Unknown of string // any other content (should not happen)
 
     // Try match a HTML tag with html
     let (|Tag|_|) html =
@@ -180,10 +182,54 @@ module WebAgility =
                 Some(NodeDefinition(name, attrs, autoClosed), afterDefinition)
         | _                 -> None
 
+    let ReadScriptComment html =
+        let (commentSep, commentSepAfter) =
+            match html with
+            | Prefix "//" after -> ("\n", after)
+            | Prefix "/*" after -> ("*/", after)
+            | _                 -> (System.String.Empty, html)
+
+        let rec ReadScriptDelimitedComment sep html =
+            match html with
+            | Prefix sep after              -> (sep, after)
+            | PrefixFirst (first, after)    ->
+                let (comment, commentAfter) = ReadScriptDelimitedComment sep after
+                (first + comment, commentAfter)
+            | _                             -> (System.String.Empty, System.String.Empty)
+
+        ReadScriptDelimitedComment commentSep commentSepAfter
+
+    let (|ScriptString|_|) script =
+        match script with
+        | Prefix "'" after
+        | Prefix @"""" after    -> Some(ReadString script)
+        | _                     -> None
+
+    let (|ScriptComment|_|) script =
+        match script with
+        | Prefix "/*" after
+        | Prefix "//" after -> Some(ReadScriptComment script)
+        | _                 -> None
+
+    // Reads a script tag content from html which one may contain // or /* */ comments
+    let rec ReadScript html =
+        match html with
+        | Prefix "<" after                  -> (System.String.Empty, html)
+        | ScriptString (str, after)         ->
+            let (script, scriptAfter) = ReadScript after
+            (str + script, scriptAfter)
+        | ScriptComment (comment, after)    ->
+            let (script, scriptAfter) = ReadScript after
+            (comment + script, scriptAfter)
+        | PrefixFirst (first, after)        ->
+            let (script, scriptAfter) = ReadScript after
+            (first + script, scriptAfter)
+        | _                                 -> (System.String.Empty, System.String.Empty)
+
     // Try match a html fragment with an between-nodes literal
     // eg: <title>Page title</title> contains the "Page title" literal
     let (|Literal|_|) html =
-       match html with
+        match html with
         | Prefix "<" after                  -> None
         | Contains "<" (before, after)      -> Some(HtmlNode.Literal(before), "<" + after)
         | _                                 -> None
@@ -191,7 +237,19 @@ module WebAgility =
     // Iterate through html and yield a node whenever discovered
     let rec HtmlNodes html =
         match html with
-        | Tag (node, after)
+        | Tag (node, after)             -> seq {
+                                                yield node
+                                                match node with
+                                                // Try match <script> special tag which may contain html tag included in comments or strings
+                                                |   NodeDefinition("script", _, _)  ->
+                                                    let (script, scriptAfter) = ReadScript after
+                                                    match script with
+                                                    | ""    -> () |> ignore
+                                                    | _     -> yield HtmlNode.Literal(script)
+                                                    yield! HtmlNodes scriptAfter
+                                                | _                                 ->
+                                                    yield! HtmlNodes after
+                                            }
         | Literal (node, after)         -> seq {
                                                 yield node
                                                 yield! HtmlNodes after
@@ -200,17 +258,27 @@ module WebAgility =
         | _                             -> Seq.empty
 
     // Provides a type for a HTML tag (eg: <p style="..."> ...</p>)
-    type HtmlTag = { Name:string; Attributes:Attribute list; Parent:HtmlTag option}
+    type HtmlTag(name:string, attrs:Attribute list, parent:HtmlTag option) =
+        new(name) = HtmlTag(name, [], None)
+        new(name, attrs) = HtmlTag(name, attrs, None)
+
+        member this.Name  = name
+        member this.Attributes = attrs
+        member this.Parent = parent
+        member this.Childs = ()
+        //{ Name:string; Attributes:Attribute list; Parent:HtmlTag option}
 
     // Find all tags inside html matching tagName as name
     let HtmlTags tagName html =
         let rec FindTags parent tagName (elements:HtmlNode list) =
             match elements with
             | NodeDefinition(name, attrs, autoclosed)::after    ->
-                let tag = {Name=name; Attributes=attrs; Parent=parent}
+                let tag = HtmlTag(name, attrs, parent)
                 if name = tagName then
                     match autoclosed with
+                    // Use current parent as parent for next found tags
                     | true  -> tag::FindTags parent tagName after
+                    // Use current tag as parent for next found tags
                     | false -> tag::FindTags (Some tag) tagName after
                 else
                     FindTags (Some tag) tagName after
